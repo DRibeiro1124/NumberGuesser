2c5e35c45e861fdafed21798fb5b14229e7110c8
DRibeiro1124 <dribeiro1124@gmail.com>
2022-08-30 21:53:04 -0400

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 
 namespace numberGuesser
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            //Start with some sort of method to display a greeting for Guessing game
+        }
     }
-  }
 }

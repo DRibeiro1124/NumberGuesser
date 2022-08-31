ea2b8da5a87fbac774a991c70a017f9630352479
DRibeiro1124 <dribeiro1124@gmail.com>
2022-08-31 07:01:20 -0400

    added a greeting method

--- Program.cs
+++ Program.cs
@@ -4,9 +4,31 @@ namespace numberGuesser
 {
     class Program
     {
+        static void GameGreeting()
+        {
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Welcome to the Number Guesser Game");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+            PromptForString("What is your name?");
+            Console.WriteLine(($"Hello! Think of a number between 1 and 1024. Don't tell me your number!! I'll try to guess it correctly in 10 tries or less"));
+
+        }
+
+        static string PromptForString(string prompt)
+        {
+            Console.WriteLine(prompt);
+            var userInput = Console.ReadLine();
+            return userInput;
+
+        }
+
         static void Main(string[] args)
         {
             //Start with some sort of method to display a greeting for Guessing game
+            GameGreeting();
+
         }
     }
 }
